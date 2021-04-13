18ae3a2fb73947c8cafd2c32d817d7ec964dd95b
niwatorihada <i1986089@cse.kyoto-su.ac.jp>
2021-04-14 00:43:26 +0900

    コメントの追加とサイコロ機能の追加

--- src/python/example_bot.py
+++ src/python/example_bot.py
@@ -1,5 +1,6 @@
 import discord
 import sys
+import random
 
 filename = "../../setting.env"
 f = open(filename)
@@ -15,12 +16,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # 自分に反応しないように
     if message.author == client.user:
         return
-
+    
+    # 挨拶を返す
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
+    # サイコロ
+    if message.content.startswith('!diceroll'):
+        await message.channel.send(str(random.randint(1,6)))
+
+    # 終了コマンド
     if message.content.startswith('!bye'):
         await message.channel.send('Bye!')
         await client.logout()
